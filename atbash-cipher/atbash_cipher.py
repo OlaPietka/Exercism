2ca0d8baeccf5d37ecd23d18afcc9e8c3fc90296
OlaPietka <36880623+OlaPietka@users.noreply.github.com>
2020-03-04 20:56:32 +0100

    Add atbash cipher exercise

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,15 @@
+from string import ascii_lowercase as low
+
+
+def get_reversed(char):
+    return list(reversed(low))[low.index(char)] if char in low else char
+
+
 def encode(plain_text):
-    pass
+    plain_text = ''.join(c.lower() for c in plain_text if c.isalnum())
+    rev_text = ''.join([get_reversed(char) for char in plain_text])
+    return ' '.join(rev_text[i:i+5] for i in range(0, len(rev_text), 5))
 
 
 def decode(ciphered_text):
-    pass
+    return ''.join([get_reversed(char) for char in ciphered_text.replace(' ', '')])
