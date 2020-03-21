caec6eca5067143127472dbad03b07bbba676ada
OlaPietka <36880623+OlaPietka@users.noreply.github.com>
2020-03-22 00:01:17 +0100

    Add pythagorean triplet exercise

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,10 +1,6 @@
-def triplets_with_sum(number):
-    pass
+def triplets_with_sum(n):
+    return [[a, b, n - a - b] for a in range(n//3) for b in range(a+1, n//2) if is_triplet(a, b, n - a - b)]
 
 
-def triplets_in_range(start, end):
-    pass
-
-
-def is_triplet(triplet):
-    pass
+def is_triplet(a, b, c):
+    return a**2 + b**2 == c**2
